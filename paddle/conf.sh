3f98fb43ec3389e717b834d2d2423def2593ab23
xiaoyichao <xiao_yi_chao@163.com>
2024-03-14 21:18:38 +0800

    修改下载的模型
    Change-Id: I079382be1ed8ae81a87dc10d86833f59075c94f4

--- paddle/conf.sh
+++ paddle/conf.sh
@@ -47,8 +47,8 @@ tools=(
 
 # 模型文件列表
 models=(
-    "models/Qwen1.5-14B.tar"
-    # "models/Qwen1.5-72B-Chat-AWQ"
+    # "models/Qwen1.5-14B.tar"
+    "models/Qwen1.5-72B.tar"
     "models/Qwen1.5-72B-Chat.tar"
     # "models/Yi-34B-Chat.tar"
     # "models/Baichuan2-13B-Base.tar"
